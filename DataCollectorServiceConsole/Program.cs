53720d6258f3746869059266e40161ed19f98a00
Andrey.wys <andrey.wys@gmail.com>
2024-05-16 13:28:57 +0300

    Оптимизация

---
+++ DataCollectorServiceConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+namespace DataCollectorServiceConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new DataCollectorService.Service();
+            ServiceBase[] servicesToRun = new ServiceBase[] { service };
+
+            if (Environment.UserInteractive)
+            {
+                Console.CancelKeyPress += (x, y) => service.Stop();
+                service.Start();
+
+                Console.WriteLine("Служба запущена. Нажмите любую кнопку для ее остановки");
+
+                Console.ReadKey();
+                service.Stop();
+                Console.WriteLine("Служба остановлена");
+            }
+            else
+            {
+                ServiceBase.Run(servicesToRun);
+            }
+        }
+    }
+}
